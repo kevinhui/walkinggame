595725e42a363258512ca69746fd0a20acaaa3a7
kevinhuijoyaether <kevinhui@joyaether.com>
2015-03-30 16:27:51 +0800

    16:27 3/30/2015

--- core/src/com/mygdx/game/Entity.java
+++ core/src/com/mygdx/game/Entity.java
@@ -1,8 +1,19 @@
 package com.mygdx.game;
 
 public class Entity {
+    private String name;
     private int health;
     private int strength;
-    private int speed;
+    private int toughness;
+    private int dexterity;
     private int concentration;
+
+    public Entity(String name, int health, int strength, int toughness, int dexterity, int concentration) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.toughness = toughness;
+        this.dexterity = dexterity;
+        this.concentration = concentration;
+    }
 }
